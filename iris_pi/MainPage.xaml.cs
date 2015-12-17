3d36ef13eea1a70f7857fc256d17697fc1210a54
Nikola Metulev <nikolame@microsoft.com>
2015-12-17 12:43:18 +0100

    working version

--- iris_pi/MainPage.xaml.cs
+++ iris_pi/MainPage.xaml.cs
@@ -115,10 +115,9 @@ namespace iris_pi
                     var frame = args.ResultFrame;
                     var box = frame.DetectedFaces.First().FaceBox;
 
-                    var previewProperties = _mediaCapture.VideoDeviceController.GetMediaStreamProperties(MediaStreamType.VideoPreview);
-                    var previewStream = previewProperties as VideoEncodingProperties;
 
-                    if (((double)box.Height / (double)previewStream.Height) < 0.3)
+
+                    if (((double)box.Height / (double)_previewStream.Height) < 0.3)
                     {
                         UpdateUI("Hi there!", "Come closer so I can look at you better", Colors.LightGray, Colors.Black);
                         //SetStatus("Come Closer");
@@ -126,9 +125,10 @@ namespace iris_pi
                     else
                     {
                         _faceDetected = true;
+
                         if (!_analyzing) TakePhotoAndAnalyzeAsync();
                     }
-                    
+
                     Debug.WriteLine("Face Detected: Height: " + box.Height + " Width: " + box.Width);
                     
                 }
@@ -139,8 +139,9 @@ namespace iris_pi
                 }
 
             }
-            else
+            else if (!_analyzing)
             {
+                
                 UpdateUI("Welcome!", "Do you have what it takes to enter?", Colors.Black, Colors.White);
                 _faceDetected = false;
             }
@@ -148,9 +149,10 @@ namespace iris_pi
 
         #endregion Event handlers
 
+        VideoEncodingProperties _previewStream;
 
         #region MediaCapture methods
-        
+
         private async Task InitializeCameraAsync()
         {
             Debug.WriteLine("InitializeCameraAsync");
@@ -178,8 +180,9 @@ namespace iris_pi
                     CaptureElement element = new CaptureElement();
                     element.Source = _mediaCapture;
                     await _mediaCapture.StartPreviewAsync();
-                    
-                    
+
+                    var previewProperties = _mediaCapture.VideoDeviceController.GetMediaStreamProperties(MediaStreamType.VideoPreview);
+                    _previewStream = previewProperties as VideoEncodingProperties;
 
                     var definition = new FaceDetectionEffectDefinition();
                     definition.SynchronousDetectionEnabled = false;
@@ -304,6 +307,7 @@ namespace iris_pi
                 Root.Background = new SolidColorBrush(backgroundColor);
                 MainText.Foreground = new SolidColorBrush(foregroundColor);
                 SubtitleText.Foreground = new SolidColorBrush(foregroundColor);
+                Debug.WriteLine(subttitleMessage);
             });
         }
         
